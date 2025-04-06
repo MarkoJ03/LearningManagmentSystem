9061528a90e226864a3e8a05289190d766ec0f4c
Marko Jeremic <markojeremic03@gmail.com>
2025-04-06 21:44:13 +0200

    zavrsen CRUD

--- back-end/src/main/java/server/service/KatedraService.java
+++ back-end/src/main/java/server/service/KatedraService.java
@@ -1,7 +1,6 @@
 package server.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
