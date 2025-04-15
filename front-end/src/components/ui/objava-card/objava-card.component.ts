18faa33de86a554f597caa9dfb82236c30cc2617
srdjan-kolcek <skolcek@gmail.com>
2025-04-15 17:50:41 +0200

    Dodao HTML i CSS za objave na Homepage i User header

---
+++ front-end/src/components/ui/objava-card/objava-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-objava-card',
+  imports: [],
+  templateUrl: './objava-card.component.html',
+  styleUrl: './objava-card.component.css'
+})
+export class ObjavaCardComponent {
+
+}
