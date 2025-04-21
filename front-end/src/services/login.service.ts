a3efc0da43d60eb40588ef12cb92365f01a86f5e
srdjan-kolcek <skolcek@gmail.com>
2025-04-21 13:11:10 +0200

    Login with Ivan server

---
+++ front-end/src/services/login.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { 
+    let t = localStorage.getItem("token");
+    if(t) {
+      this.token = t;
+    }
+  }
+
+  token: any = null;
+
+  login(user: any) {
+    //kada uvedemo model korisnika trebalo bi umesto any
+    //valjda
+    return this.http.post<any>("http://localhost:3000/login", user).pipe(
+      tap(x => {
+        this.token = x["token"];
+        localStorage.setItem("token", this.token);
+      })
+    );
+  }
+
+  getUser() {
+    if(this.token) {
+      let payload = this.token.split(".")[1];
+      return JSON.parse(atob(payload));
+    }
+  }
+
+  getRoles() {
+    let user = this.getUser();
+    if(user) {
+      return user.roles;
+    }
+  }
+
+  validateRoles(requiredRoles: any) {
+    let requiredRolesSet: any = new Set(requiredRoles);
+    let userRolesSet: any = new Set(this.getRoles());
+    let r = requiredRolesSet.intersection(userRolesSet);
+    if(r.size > 0) {
+      return true;
+    }
+    return false;
+  }
+}
