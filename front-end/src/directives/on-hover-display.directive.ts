d41d408dff7743562038a9cb81e978300b2787f5
Goran <goransokic34@gmail.com>
2025-04-23 11:29:25 +0200

    Ispravljen header sa onClick

--- front-end/src/directives/on-hover-display.directive.ts
+++ front-end/src/directives/on-hover-display.directive.ts
@@ -1,32 +1,51 @@
-import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appOnHoverDisplay]'
 })
-export class OnHoverDisplayDirective implements OnInit{
-  private originalDisplay: any;
-  constructor(private element: ElementRef) { }
+export class OnHoverDisplayDirective {
+  private static openDiv: HTMLElement | null = null; 
+  private isVisible = false;
+  private childDiv: HTMLElement | null = null;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    const childDiv = this.element.nativeElement.querySelector('div');
-    if (childDiv) {
-      this.originalDisplay = childDiv.style.display;
+    this.childDiv = this.el.nativeElement.querySelector('div');
+    if (this.childDiv) {
+      this.renderer.setStyle(this.childDiv, 'display', 'none');
     }
   }
 
-  @HostListener("mouseenter")
-  onMouseEnter(){
-    const childDiv = this.element.nativeElement.querySelector('div');
-    if (childDiv) {
-      childDiv.style.display = 'block'; 
-    }
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', '#f0f0f0');
   }
-  
-  @HostListener("mouseleave")
-  onMouseLeave(){
-    const childDiv = this.element.nativeElement.querySelector('div');
-    if (childDiv) {
-      childDiv.style.display = this.originalDisplay;
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.renderer.removeStyle(this.el.nativeElement, 'background-color');
+  }
+
+  @HostListener('click')
+  onClick() {
+    //Za zatvaranje prethodnog
+    if (OnHoverDisplayDirective.openDiv && OnHoverDisplayDirective.openDiv !== this.childDiv) {
+      this.renderer.setStyle(OnHoverDisplayDirective.openDiv, 'display', 'none');
+    }
+
+   
+    this.isVisible = !this.isVisible;
+    const displayValue = this.isVisible ? 'block' : 'none';
+    if (this.childDiv) {
+      this.renderer.setStyle(this.childDiv, 'display', displayValue);
+    }
+
+    
+    if (this.isVisible) {
+      OnHoverDisplayDirective.openDiv = this.childDiv;
+    } else {
+      OnHoverDisplayDirective.openDiv = null;
     }
   }
 }
