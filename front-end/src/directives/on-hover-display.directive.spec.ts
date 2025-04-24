782244285b04ef46c34e85296352b0c64503a834
Marko Jeremic <markojeremic03@gmail.com>
2025-04-24 15:16:11 +0200

    obrisani nepotrebni folderi

--- front-end/src/directives/on-hover-display.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnHoverDisplayDirective } from './on-hover-display.directive';
-
-describe('OnHoverDisplayDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnHoverDisplayDirective();
-    expect(directive).toBeTruthy();
-  });
-});
